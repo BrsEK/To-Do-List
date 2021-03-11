c51258b8347b1dbe067e2c265035399ee69a12cd
Boris <krinitsky.B@yandex.ru>
2021-03-12 02:12:29 +0300

    added HomeController for routing html pages

---
+++ src/main/java/com/krinitsky/todolist/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.krinitsky.todolist.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showIndexPage(){
+        return "index";
+    }
+
+}
