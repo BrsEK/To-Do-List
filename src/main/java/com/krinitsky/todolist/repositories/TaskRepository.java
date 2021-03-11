e1a313427318e2285fe3f4eed685472d7902c1dd
Boris <krinitsky.B@yandex.ru>
2021-03-11 16:09:09 +0300

    added  method for finding by text

--- src/main/java/com/krinitsky/todolist/repositories/TaskRepository.java
+++ src/main/java/com/krinitsky/todolist/repositories/TaskRepository.java
@@ -4,5 +4,8 @@ package com.krinitsky.todolist.repositories;
 import com.krinitsky.todolist.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task>findByText(String text);
 }
