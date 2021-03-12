d9466c9109ad2ef6fe6ca45329d650643f598950
Boris <krinitsky.B@yandex.ru>
2021-03-12 19:53:16 +0300

    added method addTask

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,6 +5,10 @@ Vue.createApp({
       needDoList: [],
       completeList: [],
       tasks: [],
+      task: {
+        text: null,
+        isPerformed: false
+      }
     };
   },
   mounted: function () {
@@ -16,6 +20,28 @@ Vue.createApp({
       .catch(function (e) {
         this.error = e;
       });
+  },
+
+  methods: {
+    handleInput (event) {
+        this.valueInput = event.target.value;
+    },
+
+    addTask() {
+      if(this.valueInput===""){ return };
+      axios
+        .post("/api/tasks/", {
+          text: this.valueInput,
+          isPerformed: false
+        })
+        .then(response => {
+          this.tasks.push(response.data)
+        })
+        .catch(function (e) {
+          this.error = e;
+        });
+        this.valueInput = "";
+    }
   },
 }
 ).mount('#app');
